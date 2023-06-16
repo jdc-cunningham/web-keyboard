c400738d1c1caf8a539244a97094ddc2d12a4b75
Jacob David Cunningham <jcun4128@gmail.com>
2023-06-16 10:45:43 -0500

    start key mapping

---
+++ src/components/web-keyboard/WebKeyboard.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import './WebKeyboard.css';
+import { keys } from './key-map';
+
+function WebKeyboard() {
+  
+  return (
+    <div className="WebKeyboard">
+      
+    </div>
+  );
+}
+
+export default WebKeyboard;
---
+++ src/components/web-keyboard/key-map.js
@@ -0,0 +1,210 @@
+export const keys = {
+  'a': {
+    vals: ['a'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'b': {
+    vals: ['b'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'c': {
+    vals: ['c'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'd': {
+    vals: ['d'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'e': {
+    vals: ['e'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'f': {
+    vals: ['f'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'g': {
+    vals: ['g'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'h': {
+    vals: ['h'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'i': {
+    vals: ['i'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'j': {
+    vals: ['j'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'k': {
+    vals: ['k'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'l': {
+    vals: ['l'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'm': {
+    vals: ['m'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'n': {
+    vals: ['n'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'o': {
+    vals: ['o'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'p': {
+    vals: ['p'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'q': {
+    vals: ['q'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'r': {
+    vals: ['r'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  's': {
+    vals: ['s'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  't': {
+    vals: ['t'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'u': {
+    vals: ['u'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'v': {
+    vals: ['v'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'w': {
+    vals: ['w'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'x': {
+    vals: ['x'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'y': {
+    vals: ['y'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+  'z': {
+    vals: ['z'],
+    loc: {
+      x: 0,
+      y: 0,
+      rotation: ''
+    },
+  },
+};
